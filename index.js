4577868df44bf92d2b59e7c86716f0fb7eba98d0
renorari <renorari@renorari.net>
2023-08-19 23:34:19 +0900

    Fix all auto fixable problems

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-const WebSocket = require("ws")
+const WebSocket = require("ws");
 const server = new WebSocket.Server({
-  port: 3001
+    port: 3001
 });
 
-console.log("シグナリングサーバーを起動しました")
+console.log("シグナリングサーバーを起動しました");
 
 let sockets = [];
 server.on("connection",(socket)=>{
-  sockets.push(socket)
+    sockets.push(socket);
 
-  socket.on("message",(message)=>{
-    sockets
-      .filter((s)=>s !== socket)
-      .forEach((s)=>s.send(message))
-  })
+    socket.on("message",(message)=>{
+        sockets
+            .filter((s)=>s !== socket)
+            .forEach((s)=>s.send(message));
+    });
 
-  socket.on("close",()=>{
-    sockets = sockets.filter((s)=>s !== socket)
-  })
-})
+    socket.on("close",()=>{
+        sockets = sockets.filter((s)=>s !== socket);
+    });
+});
