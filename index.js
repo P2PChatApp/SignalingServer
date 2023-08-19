92ed84ebbda75fc1c357565117bfaceec6ae5cb6
renorari <renorari@renorari.net>
2023-08-20 00:12:21 +0900

    set indent 2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const log4js = require("log4js");
 require("dotenv").config();
 const PORT = process.env.PORT || 3001;
 const server = new WebSocket.Server({
-    port: PORT,
+  port: PORT,
 });
 const log = log4js.getLogger();
 log.level = process.env.LOG_LEVEL || "info";
@@ -12,18 +12,18 @@ log.info(`Server running at ws://localhost:${PORT}`);
 
 let sockets = [];
 server.on("connection", (socket) => {
-    log.trace("New connection");
-    sockets.push(socket);
+  log.trace("New connection");
+  sockets.push(socket);
 
-    socket.on("message", (message) => {
-        log.trace(`Message received: ${message}`);
-        sockets
-            .filter((s) => s !== socket)
-            .forEach((s) => s.send(message));
-    });
+  socket.on("message", (message) => {
+    log.trace(`Message received: ${message}`);
+    sockets
+      .filter((s) => s !== socket)
+      .forEach((s) => s.send(message));
+  });
 
-    socket.on("close", () => {
-        log.trace("Connection closed");
-        sockets = sockets.filter((s) => s !== socket);
-    });
+  socket.on("close", () => {
+    log.trace("Connection closed");
+    sockets = sockets.filter((s) => s !== socket);
+  });
 });
