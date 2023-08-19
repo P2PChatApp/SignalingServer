776583dea90db36e4f260d16c9d6f4ce0915b3f4
renorari <renorari@renorari.net>
2023-08-19 23:41:19 +0900

    add log, port, env

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
 const WebSocket = require("ws");
+const log4js = require("log4js");
+require("dotenv").config();
+const PORT = process.env.PORT || 3001;
 const server = new WebSocket.Server({
-    port: 3001
+    port: PORT,
 });
+const log = log4js.getLogger();
+log.level = process.env.LOG_LEVEL || "info";
 
-console.log("シグナリングサーバーを起動しました");
+log.info(`Server running at ws://localhost:${PORT}`);
 
 let sockets = [];
-server.on("connection",(socket)=>{
+server.on("connection", (socket) => {
+    log.trace("New connection");
     sockets.push(socket);
 
-    socket.on("message",(message)=>{
+    socket.on("message", (message) => {
+        log.trace(`Message received: ${message}`);
         sockets
-            .filter((s)=>s !== socket)
-            .forEach((s)=>s.send(message));
+            .filter((s) => s !== socket)
+            .forEach((s) => s.send(message));
     });
 
-    socket.on("close",()=>{
-        sockets = sockets.filter((s)=>s !== socket);
+    socket.on("close", () => {
+        log.trace("Connection closed");
+        sockets = sockets.filter((s) => s !== socket);
     });
 });
