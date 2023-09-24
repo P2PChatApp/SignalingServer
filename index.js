d7b0f7547adbbd7c3546259a51a18fbf807a1ada
Taka005 <01taka005@gmail.com>
2023-09-24 20:04:53 +0900

    change

--- index.js
+++ index.js
@@ -1,29 +1,47 @@
 const WebSocket = require("ws");
-const log4js = require("log4js");
 require("dotenv").config();
-const PORT = process.env.PORT || 3001;
+const parse = require("./lib/parse");
+
 const server = new WebSocket.Server({
-  port: PORT,
+  port: 3000
 });
-const log = log4js.getLogger();
-log.level = process.env.LOG_LEVEL || "info";
 
-log.info(`Server running at ws://localhost:${PORT}`);
+console.log("起動しました");
 
-let sockets = [];
-server.on("connection", (socket) => {
-  log.trace("New connection");
-  sockets.push(socket);
+let peers = [];
+server.on("connection",(socket)=>{
+  console.log("接続");
 
-  socket.on("message", (message) => {
-    log.trace(`Message received: ${message}`);
-    sockets
-      .filter((s) => s !== socket)
-      .forEach((s) => s.send(message));
+  peers.push({
+    socket: socket
   });
 
-  socket.on("close", () => {
-    log.trace("Connection closed");
-    sockets = sockets.filter((s) => s !== socket);
+  const timeout = setTimeout(()=>{
+    peers = peers.filter(p=>p.socket !== socket);
+  },5000);
+
+  socket.on("message",(_data)=>{
+    const data = parse(_data.toString());
+    if(!data) return;
+    console.log(`${data}`);
+
+    if(data.event === "AUTH"){
+      peers[peers.findIndex(p=>p.socket !== socket)].address = data.data;
+    }else{
+      const peer = peers.find(p=>p.address === data.address);
+      if(peer){
+        peer.socket.send(JSON.stringify(data));
+      }else{
+        peers
+          .filter(p=>p.socket !== socket)
+          .forEach(p=>p.socket.send(JSON.stringify(data)))
+      }
+
+    }
+  });
+
+  socket.on("close",()=>{
+    console.log("切断");
+    peers = peers.filter(p=>p.socket !== socket);
   });
 });
