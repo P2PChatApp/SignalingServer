a92bcd98b22785ed54227549bd1b0868aa72654d
Taka005 <01taka005@gmail.com>
2023-09-25 11:02:55 +0900

    fix

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ console.log("起動しました");
 
 let peers = [];
 server.on("connection",(socket)=>{
-  console.log("接続");
+  console.log(`接続: ${socket.remoteAddress}`);
 
   peers.push({
     socket: socket
@@ -23,15 +23,17 @@ server.on("connection",(socket)=>{
   socket.on("message",(_data)=>{
     const data = parse(_data.toString());
     if(!data) return;
-    console.log(`${data}`);
 
     if(data.event === "AUTH"){
+      console.log(`認証: ${data.data}(${socket.remoteAddress})`);
       peers[peers.findIndex(p=>p.socket !== socket)].address = data.data;
     }else{
       const peer = peers.find(p=>p.address === data.address);
       if(peer){
+        console.log(`転送(${peer.socket.remoteAddress}): ${data}`);
         peer.socket.send(JSON.stringify(data));
       }else{
+        console.log(`送信: ${data}`);
         peers
           .filter(p=>p.socket !== socket&&p.address)
           .forEach(p=>p.socket.send(JSON.stringify(data)))
@@ -40,7 +42,7 @@ server.on("connection",(socket)=>{
   });
 
   socket.on("close",()=>{
-    console.log("切断");
+    console.log(`切断: ${socket.remoteAddress}`);
     peers = peers.filter(p=>p.socket !== socket);
   });
 });
